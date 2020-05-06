1d214123e7c100089789410ca3421fde9eae81e0
mbcdev001 <mbcdev001@gmail.com>
2020-05-06 20:50:35 +0530

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    mode: 'development',
+    entry: ['./src/index.js'],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/'
+    },
+    resolve: { extensions: ['*', '.js', '.jsx'] },
+    module:{
+        rules:[
+            {  
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(css)$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins:[ new webpack.HotModuleReplacementPlugin()],
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        port: 3000,
+        host: 'localhost',
+        open: true,
+        hotOnly: true,
+        stats: 'errors-only',
+        quiet: true
+        // proxy: {
+        //     '*': 'http://localhost:3004/'
+        // }
+    }
+}
